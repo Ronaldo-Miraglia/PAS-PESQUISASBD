904e62851d269d1d58564d5425e0c648f2dec4fd
Ronaldo Miraglia <ronaldopraia@hotmail.com>
2018-01-26 08:51:24 -0300

    Add files via upload
    Procedimento realizados no ano de 2017

---
+++ procedimento_realizado.sql
@@ -0,0 +1,7 @@
+select *
+from sipas.procedimento p
+join sipas.procedimento_executado pe ON p.id = pe.id_procedimento
+where pe.status_servico = 'REALIZADO'
+and pe.quantidade_realizada >= 1
+and pe.data_hora_termino >= '2017-01-01' and pe.data_hora_termino < '2018-01-01'
+and p.codigo = '71200029'
